2b085489067facc5d1f971b670f524ea2e49c7cc
3phase <pbozhinow@gmail.com>
2021-12-06 14:58:54 +0200

    chore(*): Sample to reproduce issue with transactions

--- src/app/grid/grid.sample.ts
+++ src/app/grid/grid.sample.ts
@@ -1,294 +1,126 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import {
-    IgxColumnComponent,
-    IgxGridComponent,
-    IgxSnackbarComponent,
-    IgxToastComponent,
-    SortingDirection,
-    CsvFileTypes,
-    IgxBaseExporter,
-    IgxCsvExporterOptions,
-    IgxCsvExporterService,
-    IgxExcelExporterOptions,
-    IgxExporterOptionsBase,
-    IgxExcelExporterService,
-    IgxStringFilteringOperand,
-    DefaultSortingStrategy,
-    GridSelectionMode
-} from 'igniteui-angular';
-import { RemoteService } from '../shared/remote.service';
-import { LocalService } from '../shared/local.service';
+
+import { Component, ViewChild } from '@angular/core';
+import { IgxDialogComponent, IgxGridComponent, Transaction } from 'igniteui-angular';
+import { FinancialDataService } from '../shared/financialData';
 
 
 @Component({
     selector: 'app-grid-sample',
-    styleUrls: [ 'grid.sample.css'],
+    styleUrls: ['grid.sample.css'],
     templateUrl: 'grid.sample.html'
 })
-export class GridSampleComponent implements OnInit, AfterViewInit {
+export class GridSampleComponent {
     @ViewChild('grid1', { static: true })
-    private grid1: IgxGridComponent;
+    public grid1: IgxGridComponent;
 
-    @ViewChild('grid2', { static: true })
-    private grid2: IgxGridComponent;
+    @ViewChild(IgxDialogComponent, { static: true })
+    public dialog: IgxDialogComponent;
 
-    @ViewChild('grid3', { static: true })
-    private grid3: IgxGridComponent;
 
-    @ViewChild('toast', { static: true })
-    private toast: IgxToastComponent;
+    public data: any[];
 
-    @ViewChild('snax', { static: true })
-    private snax: IgxSnackbarComponent;
+    public transactionsData: Transaction[] = [];
 
-    public data: Observable<any[]>;
-    public remote: Observable<any[]>;
-    public localData: any[];
-    public selectedCell;
-    public selectedRow;
-    public newRecord = '';
-    public editCell;
-    public exportFormat = 'XLSX';
-    public customFilter = CustomStringFilter.instance();
-    public selectionMode;
 
-    constructor(private localService: LocalService,
-                private remoteService: RemoteService,
-                private excelExporterService: IgxExcelExporterService,
-                private csvExporterService: IgxCsvExporterService) {
-
-        this.localService.url = this.remoteService.url;
-
-        this.remoteService.urlBuilder = (dataState) => {
-            let qS = '';
-            if (dataState && dataState.paging) {
-                const skip = dataState.paging.index * dataState.paging.recordsPerPage;
-                const top = dataState.paging.recordsPerPage;
-                qS += `$skip=${skip}&$top=${top}&$count=true`;
-            }
-            if (dataState && dataState.sorting) {
-                const s = dataState.sorting;
-                if (s && s.expressions && s.expressions.length) {
-                    qS += (qS ? '&' : '') + '$orderby=';
-                    s.expressions.forEach((e, ind) => {
-                        qS += ind ? ',' : '';
-                        qS += `${e.fieldName} ${e.dir === SortingDirection.Asc ? 'asc' : 'desc'}`;
-                    });
-                }
-            }
-            qS = qS ? `?${qS}` : '';
-            return `${this.remoteService.url}${qS}`;
-        };
+    constructor(private localService: FinancialDataService) {
+        this.localService.getData(1000000);
+        this.localService.records.subscribe(data => {
+            this.data = data;
+        })
     }
 
-    public ngOnInit(): void {
-        this.data = this.localService.records;
-        this.remote = this.remoteService.remoteData;
-
-        this.localService.getData();
-
-        this.localData = [
-            { ID: 1, Name: 'A' },
-            { ID: 2, Name: 'B' },
-            { ID: 3, Name: 'C' },
-            { ID: 4, Name: 'D' },
-            { ID: 5, Name: 'E' }
-        ];
-
-        this.grid2.sortingExpressions = [];
-        this.grid3.sortingExpressions = [{ fieldName: 'ProductID', dir: SortingDirection.Desc, ignoreCase: true,
-            strategy: DefaultSortingStrategy.instance() }];
-
-        this.selectionMode = GridSelectionMode.multiple;
+    public formatNumber(value: number) {
+        return value.toFixed(2);
+    }
+    public formatCurrency(value: number) {
+        return '$' + value.toFixed(2);
     }
 
-    public ngAfterViewInit() {
-        this.grid1.cdr.detectChanges();
-        // this.remoteService.getData(this.grid3.data);
+    public undo() {
+        /* exit edit mode and commit changes */
+        this.grid1.endEdit(true);
+        this.grid1.transactions.undo();
     }
 
-
-    public onInlineEdit(event) {
-        this.editCell = event.cell;
+    public redo() {
+        /* exit edit mode and commit changes */
+        this.grid1.endEdit(true);
+        this.grid1.transactions.redo();
     }
 
-    public showInput(index, field) {
-        return this.editCell && this.editCell.columnField === field && this.editCell.rowIndex === index;
+    public openCommitDialog() {
+        this.transactionsData = this.grid1.transactions.getAggregatedChanges(true);
+        this.dialog.open();
     }
 
-    public process() {
-        this.toast.position = 'bottom';
-        this.toast.open('Loading remote data');
-        this.remoteService.getData(this.grid3.data, () => {
-            this.toast.close();
-        });
+    public commit() {
+        this.grid1.transactions.commit(this.data);
+        this.dialog.close();
     }
 
-    public initColumns(event: IgxColumnComponent) {
-        const column: IgxColumnComponent = event;
-        if (column.field === 'Name') {
-            column.filterable = true;
-            column.sortable = true;
-            column.editable = true;
-        }
-    }
-
-    public onPagination(event) {
-        const total = this.grid2.data.length;
-        const state = this.grid2.pagingState;
-        if ((state.recordsPerPage * event) >= total) {
-            return;
-        }
-        this.grid2.paginator.paginate(event);
-    }
-
-    public onPerPage(event) {
-        const total = this.grid2.data.length;
-        const state = this.grid2.pagingState;
-        if ((state.index * event) >= total) {
-            return;
-        }
-        this.grid2.perPage = event;
-        state.paging.recordsPerPage = event;
-    }
-
-    public selectCell(event) {
-        this.selectedCell = event;
+    public discard() {
+        this.grid1.transactions.clear();
+        this.dialog.close();
     }
 
     public addRow() {
-        if (!this.newRecord.trim()) {
-            this.newRecord = '';
-            return;
-        }
-        const record = { ID: this.grid1.data[this.grid1.data.length - 1].ID + 1, Name: this.newRecord };
-        this.grid1.addRow(record);
-        this.newRecord = '';
-    }
-
-    public updateRecord(event) {
-        const row = this.selectedCell.cell.row;
-        this.grid1.updateCell(event, row.key, this.selectedCell.cell.column.field);
-    }
-
-    public deleteRow() {
-        if (this.selectedCell.cell) {
-            const row = this.selectedCell.cell.row;
-            row.delete();
-            this.snax.open(`Row with ID ${row.index} was deleted`);
-            this.selectedCell = {};
-        }
-    }
-
-    public restore() {
-        this.grid1.addRow(this.selectedRow.record);
-        this.snax.close();
-    }
-
-    public updateRow11() {
-        this.grid3.updateRow({
-            __metadata: {
-                uri: 'http://services.odata.org/Northwind/Northwind.svc/Products(20)',
-                type: 'NorthwindModel.Product'
-            },
-            ProductName: 'Example Change',
-            ProductID: 12,
-            SupplierID: 8,
-            CategoryID: 3,
-            QuantityPerUnit: undefined,
-            UnitsInStock: -99,
-            UnitsOnOrder: 0,
-            ReorderLevel: -12,
-            Discontinued: false,
-            OrderDate: new Date('1905-03-17'),
-            Category: {
-                __deferred: {
-                    uri: 'http://services.odata.org/Northwind/Northwind.svc/Products(20)/Category'
-                }
-            },
-            Order_Details: {
-                __deferred: {
-                    uri: 'http://services.odata.org/Northwind/Northwind.svc/Products(20)/Order_Details'
-                }
-            },
-            Supplier: {
-                __deferred: {
-                    uri: 'http://services.odata.org/Northwind/Northwind.svc/Products(20)/Supplier'
-                }
-            }
-        }, 11);
-    }
-    public exportRaw() {
-        this.getExporterService().export(this.grid3, this.getOptions('Report'));
-    }
-
-    public export() {
-        this.grid3.clearFilter();
-
-        const options = this.getOptions('Report');
-        options.ignoreColumnsVisibility = false;
-
-        this.getExporterService().export(this.grid3, options);
-    }
-
-    public exportFilteredGrid() {
-        this.grid3.filter('ProductName', 'Queso', IgxStringFilteringOperand.instance().condition('contains'), true);
-        this.grid3.cdr.detectChanges();
-
-        const options = this.getOptions('Queso Report');
-        options.ignoreFiltering = false;
-        options.ignoreColumnsVisibility = false;
-
-        this.getExporterService().export(this.grid3, options);
-    }
-    public exportData() {
-        this.getExporterService().exportData(this.grid3.data, this.getOptions('Data'));
-    }
-
-    public onSearchChange(text: string) {
-        this.grid3.findNext(text);
-    }
-
-    public onSearch(ev) {
-        if (ev.key === 'Enter' || ev.key === 'ArrowDown'  || ev.key === 'ArrowRight') {
-            this.grid3.findNext(ev.target.value);
-        } else if (ev.key === 'ArrowUp' || ev.key === 'ArrowLeft') {
-            this.grid3.findPrev(ev.target.value);
-        }
-    }
-
-    public columnInit(column: IgxColumnComponent) {
-        column.editable = true;
-    }
-
-    private getExporterService(): IgxBaseExporter {
-        return this.exportFormat === 'XLSX' ? this.excelExporterService : this.csvExporterService;
-    }
-
-    private getOptions(fileName: string): IgxExporterOptionsBase {
-        switch (this.exportFormat) {
-            case 'XLSX':
-                return new IgxExcelExporterOptions(fileName);
-            case 'CSV':
-                return new IgxCsvExporterOptions(fileName, CsvFileTypes.CSV);
-            case 'TSV':
-                return new IgxCsvExporterOptions(fileName, CsvFileTypes.TSV);
-            case 'TAB':
-                return new IgxCsvExporterOptions(fileName, CsvFileTypes.TAB);
-        }
-    }
-}
-
-export class CustomStringFilter extends IgxStringFilteringOperand {
-
-    private constructor() {
-        super();
-        this.append({
-            name: 'Custom',
-            logic: (value: any, searchVal: any) => value === searchVal,
-            isUnary: false,
-            iconName: 'starts_with'
+        this.grid1.addRow({
+            'Buy': 27.55,
+            'BuyDiff': '-16.31617113850485',
+            'CUSIP': "1765866",
+            'Category': "Oil",
+            'Change': '0.23000000000000043',
+            'Change On Year(%)': 9.0119,
+            'Change(%)': 0.84,
+            'CollatT': "NEW MONEY",
+            'Contract': "Forwards",
+            'Country': "Israel",
+            'Cpn': "7.875",
+            'CpnTyp': "FIXED",
+            'Curncy': "USD",
+            'DBRS': "N.A.",
+            "Fitch": "N.A.",
+            'High(D)': 27.55,
+            'High(D)Diff': "-16.31617113850485",
+            'High(Y)': 29.32,
+            'High(Y)Diff': 946.3379260333586,
+            'ID': -1,
+            'IndGrou': "Airlines",
+            'IndSect': "Consumer, Cyclical",
+            'IndSubg': "Airlines",
+            'IssuerN': "AMERICAN AIRLINES GROUP",
+            'KRD_1YR': -0.00187,
+            'KRD_3YR': 0.00006,
+            'KRD_5YR': 0,
+            'LastUpdated': 'Wed Apr 21 2021 01:28:53 GMT+0300 (Eastern European Summer Time) {}',
+            'Low(D)': '27.55',
+            'Low(D)Diff': -16.31617113850485,
+            'Low(Y)': 21.28,
+            'Low(Y)Diff': -3426.3959390862933,
+            'MUNI_SECTOR': "",
+            ' Maturity': "7/13/1939",
+            'Moodys': "WR",
+            'Open Price': 27.55,
+            'OpenPriceDiff': -16.31617113850485,
+            'PD_WALA': null,
+            'Price': 27.81,
+            'RISK_COUNTRY': "",
+            'Region': "Middle East",
+            'SecType': "PUBLIC",
+            'Security': "001765866 Pfd",
+            'Sell': 27.55,
+            'SellDiff': -16.31617113850485,
+            'Settlement': "Cash",
+            'Spread': 0.02,
+            'Start(Y)': 25.3,
+            'Start(Y)Diff': -1240.0290065264674,
+            'Ticker': "AAL",
+            'Type': "Uranium",
+            'Volume': 12,
+            'ZV_SPREAD': 28.302,
+            'sector': "Pfd",
         });
+
+
     }
 }
